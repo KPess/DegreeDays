60d781e3f785fa7268263581a272d9f5587bdb47
KP <kjpessetto@gmail.com>
2021-06-10 17:44:27 -0500

    reorganized code

--- Temperature Population Data.py
+++ Temperature Population Data.py
@@ -1,1 +1,13 @@
-import pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+filename = 'Temp Data.csv'
+df_temp = pd.read_csv('../SynMax/Temp Data.csv')
+df_temp = df_temp.set_index('datetime')
+ts_tempMin = df_temp['temp_min_c']
+ts_tempMax = df_temp['temp_max_c']
+ts_tempMean = df_temp['temp_mean_c']
+
+print(ts_tempMax)
